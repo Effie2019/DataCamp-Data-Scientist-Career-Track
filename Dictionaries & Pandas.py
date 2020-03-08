f9fa48511471be37fec923126bc6f12979dc2c94
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2020-03-08 16:02:05 -0400

    Update Dictionaries & Pandas.py

--- Dictionaries & Pandas.py
+++ Dictionaries & Pandas.py
@@ -1,1 +1,37 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+# Print out the keys in europe
+print(europe.keys())
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+# Print out the capital of France
+print(europe['france']['capital'])
+# Create sub-dictionary data
+data={'capital':'rome','population':59.83}
+# Add data to europe under key 'italy'
+europe['italy']= data
+# Print europe
+print(europe)
+#CSV to DataFrame
+# Import pandas as pd
+import pandas as pd
+# Fix import by including index_col
+cars = pd.read_csv('cars.csv',index_col=0)
+# Print out cars
+print(cars)
 
+#loc&iloc
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+# Print out drives_right column as Series
+print(cars.loc[:,'drives_right'])
+# Print out drives_right column as DataFrame
+print(cars.loc[:,['drives_right']])
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:,['cars_per_cap','drives_right']])
