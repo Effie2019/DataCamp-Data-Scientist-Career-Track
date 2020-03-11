78188c5caa4352fee7e872754189f69fa594615a
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2020-03-10 22:31:21 -0400

    Update Data Manipulation with pandas.py

--- Data Manipulation with pandas.py
+++ Data Manipulation with pandas.py
@@ -41,9 +41,61 @@ sales_1_1 =sales_1_1.sort_values("date")
 sales_1_1["cum_weekly_sales"]= sales_1_1["weekly_sales"].cumsum()
 sales_1_1["cum_max_sales"]=sales_1_1["weekly_sales"].cummax()
 ##Dropping duplicates
-
+store_depts =sales.drop_duplicates(subset=["store","department"])
+holiday_dates = sales[sales["is_holiday"]==True].drop_duplicates("date")
+##Count
+sales_all = sales["weekly_sales"].sum()
+sales_A = sales[sales["type"]=="A"]["weekly_sales"].sum()
+##Group by
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+##Pivot table
+mean_sales_by_type = sales.pivot_table(values="weekly_sales",index="type")
 ###Slicing and indexing
+##index
+#Set index
+temperatures_ind = temperatures.set_index("city")
+#Reset index 
+temperatures_ind.reset_index()
+temperatures_ind.reset_index(drop=True)
+##Subsetting 
+#with .loc[]
+temperatures_ind.loc[["Moscow","Saint Petersburg"]]
+temperatures_ind.loc[[("Brazil","Rio De Janeiro"),("Pakistan","Lahore")]]
+##Sort
+#Sorting by index values
+temperatures_ind.sort_index(level=["country","city"],ascending=[True,False])
+##Slice
+#Rows
+temperatures_srt.loc[("Pakistan","Lahore"):("Russia","Moscow")]
+#Columns
+temperatures_srt.loc[:,"date":"avg_temp_c"]
+#Both directions 
+temperatures_srt.loc[("India","Hyderabad"):("Iraq","Baghdad"),"date":"avg_temp_c"]
+#.loc
+temperatures.iloc[22,1]
+##Pivot table
+temp_by_country_city_vs_year = temperatures.pivot_table("avg_temp_c",index=["country","city"],columns="year")
+
 ###Creating and Visualizing DataFrames
+##Missing Value
+#check
+print(avocados_2016.isna())
+print(avocados_2016.isna().any().sum())
+#drop
+avocados_complete = avocados_2016.dropna()
+#replace
+avocados_filled =avocados_2016.fillna(0)
+##DataFrame
+avocados_list = [
+    {"date": "2019-11-03", "small_sold": 10376832, 	"large_sold": 7835071},
+    {"date": "2019-11-10", "small_sold": 10717154, "large_sold": 8561348},
+]
+# Create a dictionary of lists with new data
+avocados_dict = {
+  "date": ["2019-11-17"	,"2019-12-01"],
+  "small_sold": [10859987, 9291631],
+  "large_sold": [7674135, 6238096]
+}
 
 
 
