c54884616e61120a6da9fde52e2f55ecabc7a9f7
Effie2019 <51808419+Effie2019@users.noreply.github.com>
2020-03-15 19:10:08 -0400

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -1,1 +1,8 @@
-
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"])
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"])
+plt.show()
+＃Customizing data appearance
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"], color='b',marker='o',linestyle='--')
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"], color='r',marker='v',linestyle='--')
